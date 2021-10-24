6d780ad50b00ffcbc6a9f19f3a5b0ed8477a5e14
Keaton Kowal <kowalk@uw.edu>
2021-10-24 09:43:07 -0700

    more setup

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+import Airtable from 'airtable'
+
 const express = require('express')
 const app = express()
 const port = 3000
+const keyObj = require('./config.json')
+const key = keyObj.key;
+const base = new Airtable({ apiKey: key }).base('appRTSRlXjQ67gLwM'); // im so sorry
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
