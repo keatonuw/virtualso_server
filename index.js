dcf4d06b3db87e8b9f7a2315196969caa8f936b4
Keaton Kowal <kowalk@uw.edu>
2021-10-24 10:03:54 -0700

    more setup

--- index.js
+++ index.js
@@ -1,15 +1,55 @@
 import Airtable from 'airtable'
+import express from 'express'
+import key from './config.js'
 
-const express = require('express')
 const app = express()
 const port = 3000
-const keyObj = require('./config.json')
-const key = keyObj.key;
-const base = new Airtable({ apiKey: key }).base('appRTSRlXjQ67gLwM'); // im so sorry
+const base = new Airtable({ apiKey: key }).base('appRTSRlXjQ67gLwM');
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  let events = waitForRecords();
+  console.log(events)
+  res.send(events)
 })
+
+async function waitForRecords() {
+  return await getRecords();
+}
+
+async function getRecords() {
+  let events = [];
+  let filterString = "";
+  base('Events').select({
+    view: "Grid view",
+    filterByFormula: filterString
+  }).eachPage((records, fetchNextPage) => {
+    // this function is called for each page of records
+    events.push.apply(recordsToEventData(records));
+    fetchNextPage(); // fetch the next one!
+  }, function done(err) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+  });
+  return events;
+}
+
+function recordsToEventData(records) {
+  let events = [];
+  records.forEach(function (record) {
+    console.log('Retrieved', record.get('Event Name'));
+    events.push({
+      name: record.get('Event Name'),
+      type: record.get('Event Type'),
+      desc: record.get('Description'),
+      image: record.get('Image'),
+      date: record.get('Date'),
+      diversity: record.get('Diversity'),
+    });
+  });
+  return events;
+}
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
